edaf5c7ee8159101d7c9bb27061bbabde480f889
AltafYafai <altaf.alu@gmail.com>
2026-09-20 03:58:04 +0530

    fix: exclude duplicate OSGi manifest from BC jars

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,13 @@ android {
         compose = true
     }
 
+    packaging {
+        resources {
+            excludes += "META-INF/versions/9/OSGI-INF/MANIFEST.MF"
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
