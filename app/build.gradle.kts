56b6e5260168efd28dab60d9f3a9d44dd136ea1b
AltafYafai <altaf.alu@gmail.com>
2026-09-20 04:02:52 +0530

    fix: exclude more duplicate META-INF files from BC jars

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,11 @@ android {
         resources {
             excludes += "META-INF/versions/9/OSGI-INF/MANIFEST.MF"
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/LICENSE.md"
+            excludes += "META-INF/NOTICE.md"
+            excludes += "META-INF/DEPENDENCIES"
+            excludes += "META-INF/LICENSE"
+            excludes += "META-INF/NOTICE"
         }
     }
 
